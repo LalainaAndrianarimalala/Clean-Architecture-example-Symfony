ff4e4c7bd5a5a7f94a9988af830b0c82588fcf68
ANDRIANARIMALALA Andry Lalaina <andrilalaina144@gmail.com>
2026-03-18 11:45:17 +0300

    feat : configuration base de donnees dans docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM dunglas/frankenphp:1.10-php8.5
 
-RUN install-php-extensions intl opcache pdo pdo_mysql mysqli
+RUN install-php-extensions \
+    intl \
+    opcache \
+    pdo \
+    pdo_mysql \
+    mysqli
 
 WORKDIR /app
--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
