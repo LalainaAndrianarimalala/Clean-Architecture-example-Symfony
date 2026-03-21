d35422fbf647840fe017c8e1cff38a7a693155cd
ANDRIANARIMALALA Andry Lalaina <andrilalaina144@gmail.com>
2026-03-21 10:03:47 +0300

    Fix : identation service fixed

---
+++ src/Domain/Utilisateur/Exception/AuthenticationFailedException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Domain\Utilisateur\Exception;
+
+class AuthenticationFailedException{
+    
+}
+
--- src/Presentation/Controller/API/UtilisateurController.php
+++ src/Presentation/Controller/API/UtilisateurController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\TooManyRequestsHttpException;
 use Symfony\Component\RateLimiter\RateLimiterFactoryInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
@@ -60,7 +60,7 @@ final class UtilisateurController extends AbstractController
                 'email'=>$utilisateur->getEmail()
             ],Response::HTTP_CREATED);
         }catch(UtilisateurAlreadyExistsException $e){
-            return $this->json(['error'=>$e->getMessage(),Response::HTTP_CONFLICT]);
+            return $this->json(['error'=>$e->getMessage()],Response::HTTP_CONFLICT);
         }
     }
 }
